364fd6ef1ae110234390bc1277d853944e02b7ae
FeliBlanco <felipeblanco09@hotmail.com>
2024-08-30 23:28:58 -0300

    cambios

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,11 +2,61 @@ import { Router } from 'express';
 import Usuario from '../models/user.js';
 import jwt from 'jsonwebtoken'
 import isLogged from '../middleware/isLogged.js';
+import { MercadoPagoConfig, Preference } from 'mercadopago'
 
 const router = Router();
 
 const generateToken = (user) => jwt.sign({ id: user._id, email: user.email }, 'feliblanco123');
 
+const clientMP = new MercadoPagoConfig({accessToken: "APP_USR-2102130027432464-083020-16e624c9994ec01ce112aa698a612cb0-544629529"});
+
+
+router.get('/compra_saldo', async(req, res) => {
+    try {
+        console.log(req.query)
+        console.log(req.params)
+        res.send()
+    }
+    catch(err) {
+        res.status(503).send()
+    }
+})
+router.post('/create_preference', isLogged, async (req, res) => {
+    try {
+        const {
+            saldo
+        } = req.body;
+
+        console.log("PREFERENCE")
+
+        const preference = new Preference(clientMP);
+
+        const body = {
+            items: [
+                {
+                    title: "Saldo",
+                    quantity: 1,
+                    unit_price: saldo,
+                    currency_id: "ARS"
+                }
+            ],
+            back_urls: {
+                success: "http://localhost",
+                failure: "http://localhos",
+                pending: "ec2-18-206-120-192.compute-1.amazonaws.com:3000/user/compra_saldo"
+            },
+            auto_return:"approved"
+        };
+
+        const result = await preference.create({body});
+        res.send({preference: result.id});
+
+    }
+    catch(err) {
+        console.log(err)
+        res.status(503).send()
+    }
+})
 router.get('/data', isLogged, async (req, res) => {
     try {
         res.send(req.user);
